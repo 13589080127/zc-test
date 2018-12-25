2cb31073809212735ac8336746b87a76480897d4
fan <fjy170@zcsmart.com>
2018-12-25 09:40:04 +0800

    Thread的特性

---
+++ src/main/java/com/zcs/test/thread/ThreadTest.java
@@ -0,0 +1,21 @@
+package com.zcs.test.thread;
+
+public class ThreadTest {
+
+
+    public static class ThisThread extends Thread{
+        ThisThread(){
+            //设置当前线程为守护线程
+            this.setDaemon(true);
+            this.start();
+        }
+        @Override
+        public void run(){
+            for (;;);
+        }
+    }
+
+    public static void main(String[] args) {
+        ThisThread thisThread = new ThisThread();
+    }
+}
