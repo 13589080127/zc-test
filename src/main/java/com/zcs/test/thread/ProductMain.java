e9d4d97423608565d479a0b4ea85fd418b949e48
fan <fjy170@zcsmart.com>
2019-01-21 17:36:45 +0800

    redis的配置，错误码内存的处理 可以自由更新

--- src/main/java/com/zcs/test/thread/ProductMain.java
+++ src/main/java/com/zcs/test/thread/ProductMain.java
@@ -3,18 +3,18 @@ package com.zcs.test.thread;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ProductMain {
-    public static LinkedBlockingQueue<String> queue = new LinkedBlockingQueue();
+    public static LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue();
     public static void main(String[] args) throws InterruptedException {
         new Consume().start();
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            add(i+"");
+            add(i);
         }
     }
     /**
      * 向队列添加的方法
      * @param ele
      */
-    public static void add(String ele) throws InterruptedException {
+    public static void add(int ele) throws InterruptedException {
         queue.put(ele);
     }
     /**
@@ -25,7 +25,7 @@ public class ProductMain {
         public void run(){
             try {
                 while (true) {
-                    String ele = queue.take();
+                    int ele = queue.take();
                     System.out.println(ele);
                 }
             } catch (InterruptedException e) {
--- src/main/java/com/zcs/test/thread/ThreadTest.java
+++ src/main/java/com/zcs/test/thread/ThreadTest.java
@@ -16,6 +16,6 @@ public class ThreadTest {
     }
 
     public static void main(String[] args) {
-        ThisThread thisThread = new ThisThread();
+        new ThisThread();
     }
 }
